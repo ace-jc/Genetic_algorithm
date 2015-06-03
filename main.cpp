c6eccb52280c8071ee6df7b7814dd11e65757191
Ace-Win <ace-jc@users.noreply.github.com>
2015-06-03 00:12:48 -0500

    misunderstood situation and action now somewhat corrected

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <stdlib.h>
+#include <time.h>
 
-#define ARRAY_SQUARE_SIZE 10
+#define ARRAY_SQUARE_SIZE 12 // without walls it is -2 in size
 #define GENE_LENGTH 243
 #define ROBBY_AMT 1
 #define CLEANING_SESSIONS 100 // per Robby
+#define SITUATIONS_ACTIONS
 
 using namespace std;
 
 class Robby{
 private:
-    int genes[GENE_LENGTH]; // array containing the 243 genes
+    char situation_table_and_genes[GENE_LENGTH][SITUATIONS_ACTIONS]; // array containing the 243 situations and genes
     int current_horizontal;
     int current_vertical;
-    int gene_read_position;
-    // genes are defined as follows:
+
+    // gene actions are defined as follows:
     // 0 is move north
     // 1 is move south
     // 2 is move east
@@ -25,16 +27,18 @@ private:
 
 public:
     Robby(){
-        initial_create_genes();
-        current_horizontal = 0;
-        current_vertical = 0;
-        gene_read_position = 0;
+        current_horizontal = 1; // to avoid wall at 0
+        current_vertical = 1; // to avoid wall at 0
+        set_up_situation_table();
     }
 
-    int read_gene(){
-        int current_gene = genes[gene_read_position];
-        gene_read_position++; // increasing the position of the gene reader
-        return current_gene;
+    void set_up_situation_table(){
+        for(int i=0; i<GENE_LENGTH; i++){
+            for(int j=0; j<SITUATIONS_ACTIONS-1; j++)
+                situation_table_and_genes[i][j] == 'r';
+                cout << situation_table_and_genes[i][j];
+        }
+        cout << endl;
     }
 
     int horizontal_position(){
@@ -43,21 +47,6 @@ public:
 
     int vertical_position(){
         return current_vertical;
-    }
-
-    void initial_create_genes(){
-        for(int i=0; i<GENE_LENGTH; i++){
-            int random_gene = rand()%7; // 0 - 6
-            genes[i] = random_gene;
-        }
-    }
-
-    //for diagnostics
-    void print_genes(){
-        cout << "Genes are: ";
-        for(int i=0; i<GENE_LENGTH; i++){
-            cout << genes[i];
-        }
     }
 
 };
@@ -79,14 +68,26 @@ public:
         this->state = 'c';
     }
 
+    void create_wall(){
+        this->state = 'w';
+    }
 };
 
 class World{
 private:
-    Items world_map_ptr[ARRAY_SQUARE_SIZE][ARRAY_SQUARE_SIZE];
+
     Robby list_of_robbies[ROBBY_AMT];
 
-    void add_cans(int cans_to_add){
+    void add_cans_and_walls(int cans_to_add){
+        // adding walls
+        for(int i=0; i<ARRAY_SQUARE_SIZE; i++){
+            for(int j=0; j<ARRAY_SQUARE_SIZE; j++){
+                if(i==0 || i==ARRAY_SQUARE_SIZE-1 || j==0 || j==ARRAY_SQUARE_SIZE-1){
+                    world_map_ptr[i][j].create_wall();
+                }
+            }
+        }
+        // adding cans
         for(int can=0; can<cans_to_add; can++){
             // will create a can for each number of cans_to_add
             int horizontal = 0;
@@ -97,12 +98,11 @@ private:
             }while(world_map_ptr[horizontal][vertical].item_state() != '.');
             world_map_ptr[horizontal][vertical].create_can();
         }
-
     }
 
 public:
     World(int cans){
-        add_cans(cans);
+        add_cans_and_walls(cans);
     }
 
     void print_world(){
@@ -118,19 +118,19 @@ public:
         // moving time forward
         int array_size = (sizeof(list_of_robbies)/ sizeof(list_of_robbies[0]));
 
-        for(int i=0; i<array_size; i++){
-            // for each Robby position
-            for(int j=0; j<CLEANING_SESSIONS; j++){
-                // all of the cleaning sessions per Robby
-                int hori = list_of_robbies[i].horizontal_position();
-                int vert = list_of_robbies[i].vertical_position();
-                int current_gene = list_of_robbies[i].read_gene();
-
-                cout << "i: " << i << " current_gene: " << current_gene << endl;
-            }
-
-
-        }
+//        for(int i=0; i<array_size; i++){
+//            // for each Robby position
+//            for(int j=0; j<CLEANING_SESSIONS; j++){
+//                // all of the cleaning sessions per Robby
+//                int hori = list_of_robbies[i].horizontal_position();
+//                int vert = list_of_robbies[i].vertical_position();
+//                int current_gene = list_of_robbies[i].read_gene();
+//
+////                cout << "i: " << i << " current_gene: " << current_gene << endl;
+//            }
+//
+//
+//        }
     }
 
 };
@@ -138,6 +138,7 @@ public:
 
 int main()
 {
+    srand(time(0));
     int can_num = 0;
     cout << "Array Square Size: " << ARRAY_SQUARE_SIZE << endl;
     do{
